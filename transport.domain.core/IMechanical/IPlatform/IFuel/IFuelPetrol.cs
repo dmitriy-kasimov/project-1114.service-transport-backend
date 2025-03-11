b1b2c7c56ce16d8b1f6036520ebdc2138a1a2b3a
Dmitriy Kasimov <its.d,itriy.kasimov@gmail.com>
2025-03-11 19:42:26 +0300

    Расширил IFuelPlatform: добавил интерфейс IFuelPetrol

---
+++ transport.domain.core/IMechanical/IPlatform/IFuel/IFuelPetrol.cs
@@ -0,0 +1,8 @@
+namespace transport.domain.core.IMechanical.IPlatform.IFuel;
+
+public interface IFuelPetrol
+{
+    public string Name { get; }
+    public string Description { get; }
+    public float Capacity { get; }
+}
--- transport.domain.core/IMechanical/IPlatform/IFuel/IFuelPlatform.cs
+++ transport.domain.core/IMechanical/IPlatform/IFuel/IFuelPlatform.cs
@@ -4,4 +4,5 @@ public interface IFuelPlatform : IPlatform
 {
     public new IFuelEngine Engine { get; set; }
     public new IFuelPowerSupply PowerSupply { get; set; }
+    public IFuelPetrol FuelPetrol {get; set; }
 }
